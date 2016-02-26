0f80b7dd978df7d22d009b2d832ca86fc832f145
tomsik68 <tomsik68@gmail.com>
2016-02-26 10:14:06 +0000

    Created templ8tor application

---
+++ src/main/java/sk/tomsik68/templator/TemplatorApplication.java
@@ -0,0 +1,10 @@
+package sk.tomsik68.templator;
+
+public final class TemplatorApplication {
+    public static void main(String[] args) {
+        if(args.length == 0) {
+            printHelp();
+            return;
+        }
+    }
+}
